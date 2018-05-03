ade6f03cfba5581e8b68b3144d205f68bcd13801
jd0824um <jd0824um@go.minneapolis.edu>
2018-05-03 12:54:35 -0500

    Created a logout page and redirected home

--- YoutubeClone/urls.py
+++ YoutubeClone/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from YoutubeClone import views
 
 urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls'))
 ]
