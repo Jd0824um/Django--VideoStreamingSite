43fd9fcfa3d36c265ded6cc6f479905c6bcb318d
jd0824um <jd0824um@go.minneapolis.edu>
2018-05-03 00:59:46 -0500

    Created model for a user

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from localflavor.us.models import USStateField
+from django.utils import timezone
+
+# Creates a user profile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=150, default='')
+    city = models.CharField(max_length=20, default='')
+    state = USStateField(null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+
+# Associates the user that's being created with the user profile
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
