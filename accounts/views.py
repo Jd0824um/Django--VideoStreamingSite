ef06b3cdb97d7d8d88c1fa05da44e86cc46c57ce
jd0824um <jd0824um@go.minneapolis.edu>
2018-05-03 13:15:28 -0500

    Created registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def home(request):
     return render(request, 'accounts/home.html')
+
+# Creates a new user from the register button
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = UserCreationForm()
+        return render(request, 'accounts/registration_form.html', {'form' : form})
